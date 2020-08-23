37a0a77fa774ca23e2b622abbcbde26ce045e8ad
Priscila Subtil <prisubtil@hotmail.com>
2020-08-23 18:54:04 -0300

    add ex-for

--- exercicio-for/src/main/Main.java
+++ exercicio-for/src/main/Main.java
@@ -13,8 +13,10 @@ public class Main {
 		System.out.println("Digite a quantidade de números que você deseja somar: ");
 		int n = sc.nextInt();
 		
+		
+		System.out.println("Digite "+n+" números: ");
+		
 		for(int i=0; i < n; i++) {
-			System.out.println("Digite um número: ");
 			int numero = sc.nextInt();
 			soma += numero;
 		}
